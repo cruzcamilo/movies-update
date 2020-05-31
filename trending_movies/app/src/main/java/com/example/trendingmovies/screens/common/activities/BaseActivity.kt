586e0f0207aefb784af25fc3eeec65759017a8d2
cruz.camilo <ccamilo_eduardo@hotmail.com>
2020-05-31 14:19:12 -0500

    Add prevention of using multiple dependency injection per class

--- trending_movies/app/src/main/java/com/example/trendingmovies/screens/common/activities/BaseActivity.kt
+++ trending_movies/app/src/main/java/com/example/trendingmovies/screens/common/activities/BaseActivity.kt
@@ -6,12 +6,18 @@ import com.example.trendingmovies.MyApplication
 import com.example.trendingmovies.common.dependencyinjection.CompositionRoot
 import com.example.trendingmovies.common.dependencyinjection.Injector
 import com.example.trendingmovies.common.dependencyinjection.PresentationCompositionRoot
+import java.lang.RuntimeException
 
 open class BaseActivity: AppCompatActivity() {
 
     private var mPresentationCompositionRoot: PresentationCompositionRoot? = null
+    private var mIsInjectionUsed = false
 
     fun getInjector(): Injector{
+        if(mIsInjectionUsed){
+            throw RuntimeException("There is no need to use injector more than use")
+        }
+        mIsInjectionUsed = true
         return Injector(getCompositionRoot())
     }
 
